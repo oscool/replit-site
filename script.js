386775b2d0abb3108ffba8a118bd6f5c4d9aaa00
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2022-01-21 14:51:20 +0000

    stuff

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ $.ajax({
     }
   })
 });
+theme = localStorage.getItem("theme")
+if (theme == null){
+  setTheme("light")
+  theme = "light"
+} 
+function setTheme(theme){
+  if (theme == "light"){
+    theme = "light"
+    localStorage.setItem("theme", "light")
+    document.body.classList = ""
+  } else if (theme == "dark"){
+    theme = "dark"
+    localStorage.setItem("theme", "dark")
+    document.body.classList = "dark-theme"
+    if (path === "https://site.oscool.repl.co/index.html"){
+      document.querySelector("button").innerText = "Use Light Mode"
+    }
+  }
+}
+setTheme(localStorage.getItem("theme"))
+function toggleTheme(){
+  if (theme == "light"){
+    theme = "dark"
+    setTheme("dark")
+    return "Use Light Mode"
+  } else if (theme == "dark"){
+    theme = "light"
+    setTheme("light")
+    return "Use Dark Mode"
+  }
+}
+function currentThemeButton(){
+  if (theme == "light"){
+    theme = "light"
+    return "Use Dark Mode"
+  } else if (theme == "dark"){
+    theme = "dark"
+    return "Use Light Mode"
+  }
+}
+function themeButton(){
+  toggleTheme()
+  return currentThemeButton()
+}
